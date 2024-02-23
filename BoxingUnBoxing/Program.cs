482e9d0bb2ad81779479e84ca14594703b9864d7
Admin <Admin@DESKTOP-7T0EOMO>
2024-02-23 23:57:35 +0530

    Boxing and UnBoxing

---
+++ BoxingUnBoxing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnBoxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=37;
+            object j = i;   //Boxing Value type to reference type  // 
+            Console.WriteLine(i+" "+j); // 37 37
+
+            // int k = j; CTE-- need Explicit
+
+            int k = (int)j;  //UnBoxing refernce type to value type
+            
+        }
+    }
+}
